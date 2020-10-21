d3180ef9e5b705b6da2f60e7af309c861945de11
Jerry Turcios <jerryturcios08@gmail.com>
2020-10-21 19:20:56 -0400

    Refactor networking logic for the avatar image view

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -50,4 +50,30 @@ class NetworkManager {
 
         dataTask.resume()
     }
+
+    func downloadAvatarImage(from urlString: String, completed: @escaping (UIImage) -> Void) {
+        guard let url = URL(string: urlString) else { return }
+        let cacheKey = NSString(string: urlString)
+
+        // Runs the completion block if a cached image has been found
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+
+        let dataTask = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            // Leave function if there is an error and use placeholder instead
+            guard let self = self else { return }
+            if error != nil { return }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+
+            // If image is available, then cache it and run the completion block
+            guard let image = UIImage(data: data) else { return }
+            self.cache.setObject(image, forKey: NSString(string: urlString))
+            completed(image)
+        }
+
+        dataTask.resume()
+    }
 }
--- GHFollowers/Subclasses/GFAvatarImageView.swift
+++ GHFollowers/Subclasses/GFAvatarImageView.swift
@@ -20,37 +20,17 @@ class GFAvatarImageView: UIImageView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    func setImage(from urlString: String) {
+        NetworkManager.shared.downloadAvatarImage(from: urlString) { [weak self] image in
+            guard let self = self else { return }
+            DispatchQueue.main.async { self.image = image }
+        }
+    }
+
     private func configureImageView() {
         translatesAutoresizingMaskIntoConstraints = false
         layer.cornerRadius = 10
         clipsToBounds = true
         image = placeholderImage
-    }
-
-    func downloadImage(from urlString: String) {
-        let cacheKey = NSString(string: urlString)
-
-        if let image = cache.object(forKey: cacheKey) {
-            self.image = image
-            return
-        }
-
-        guard let url = URL(string: urlString) else { return }
-
-        let dataTask = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-            // Leave function if there is an error; use placeholder instead
-            guard let self = self else { return }
-            if error != nil { return }
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
-            guard let data = data else { return }
-
-            // If image is available, then cache it
-            guard let image = UIImage(data: data) else { return }
-            self.cache.setObject(image, forKey: cacheKey)
-
-            DispatchQueue.main.async { self.image = image }
-        }
-
-        dataTask.resume()
     }
 }
