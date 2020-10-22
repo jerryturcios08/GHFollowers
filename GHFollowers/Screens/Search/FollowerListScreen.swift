12b7634673b982410318355bcb0748573e40b59b
Jerry Turcios <jerryturcios08@gmail.com>
2020-10-21 20:10:46 -0400

    Add loading view to UIViewController extension

--- GHFollowers/Screens/Search/FollowerListScreen.swift
+++ GHFollowers/Screens/Search/FollowerListScreen.swift
@@ -51,8 +51,11 @@ class FollowerListScreen: UIViewController {
     }
 
     private func getFollowers() {
+        showLoadingView()
+
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
 
             switch result {
             case .success(let followers):
