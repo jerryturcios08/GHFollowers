c4d3337e74f3ff177b6c0a71c95b2a4c6c1a0358
Jerry Turcios <jerryturcios08@gmail.com>
2020-10-25 20:33:33 -0400

    Implement persistence manager

--- GHFollowers/Screens/Favorites/FavoritesListScreen.swift
+++ GHFollowers/Screens/Favorites/FavoritesListScreen.swift
@@ -11,5 +11,16 @@ class FavoritesListScreen: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+
+        PersistenceManager.retrieveFavorites { result in
+//            guard let self = self else { return }
+
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
